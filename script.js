91bd1fd89f2c627894eee16e191b2930812cdde7
vlglr <98906317+vlglr@users.noreply.github.com>
2022-10-10 09:05:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function gebi(x) {
+  return document.getElementById(x);
 }
 
-var slider = document.getElementById("myRange");
-var rangeval = document.getElementById("rangeval");
+function compute() {
+    p = document.getElementById("principal").value;
+    
+    if(p<=0) {
+      alert("Enter a positive number");
+      gebi('myrez').style.display='none';
+      document.getElementById("principal").focus();
+      
+      return
+    }
+    var result1 = document.getElementById("result1");
+    result1.innerHTML = p;
+    var result2 = document.getElementById("result2");
+    result2.innerHTML = document.getElementById("rateval").innerHTML;
+    var result4 = document.getElementById("result4");
+    var years2 = document.getElementById("years2");
+    result4.innerHTML = new Date().getFullYear() + Number(years2.value);
+    var result3 = document.getElementById("result3");
+    result3.innerHTML = Number(p) * Number(document.getElementById("myRange").value) * Number(years2.value) * 0.01;
 
-rangeval.innerHTML = slider.value;
+}
 
-slider.oninput = function() {
-  output.innerHTML = this.value;
-} 
+function myrate() {
+  var slider = document.getElementById("myRange");
+  var rateval = document.getElementById("rateval");
+  rateval.innerHTML = slider.value + "%";
+}
