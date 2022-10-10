52dbc1485260ab096804e513c20a0a037c218962
vlglr <98906317+vlglr@users.noreply.github.com>
2022-10-10 06:11:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,12 @@ function compute()
     p = document.getElementById("principal").value;
     
 }
-        
+
+var slider = document.getElementById("myRange");
+var rangeval = document.getElementById("rangeval");
+
+rangeval.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+} 
